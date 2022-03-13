a81ab4fcd2533525f98d15cce70910503605cb65
Richard <au.richard4@gmail.com>
2022-03-13 15:39:05 -0700

    changed markers schema

--- db/schema/03_markers.sql
+++ db/schema/03_markers.sql
@@ -2,7 +2,8 @@ DROP TABLE IF EXISTS markers CASCADE;
 CREATE TABLE markers (
   id SERIAL PRIMARY KEY NOT NULL,
   title VARCHAR(255) NOT NULL,
-  description TEXT NOT NULL,
+  description TEXT,
+  image_url VARCHAR(255) NOT NULL,
   user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
   map_id INTEGER REFERENCES maps(id) ON DELETE CASCADE,
   latitude INTEGER NOT NULL,
