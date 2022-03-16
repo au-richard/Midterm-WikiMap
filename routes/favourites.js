1c7a38d677d262f0af19f60dc73e5811e437b6e2
Richard <au.richard4@gmail.com>
2022-03-15 17:19:45 -0700

    Changed favourites bg color. Fixed delete button routing path.

--- routes/favourites.js
+++ routes/favourites.js
@@ -5,7 +5,7 @@ module.exports = (db) => {
   router.get("/", (req, res) => {
     const userID = req.session["user_id"];
     console.log("user ID", userID);
-    db.query(`SELECT * FROM favourites JOIN users ON users.id = favourites.user_id JOIN maps ON maps.id = favourites.map_id WHERE favourites.user_id = $1 AND favourites.active = TRUE;`, [1])
+    db.query(`SELECT favourites.id, maps.title FROM favourites JOIN users ON users.id = favourites.user_id JOIN maps ON maps.id = favourites.map_id WHERE favourites.user_id = $1 AND favourites.active = TRUE;`, [userID])
       .then(data => {
         const favourites = data.rows;
         console.log("fave", favourites);
@@ -23,12 +23,14 @@ module.exports = (db) => {
     const mapID = req.params.id;
     console.log("map ID", mapID);
     console.log("req body", req.body);
+
     db.query(`UPDATE favourites SET active = FALSE WHERE id = $1`, [mapID])
       .then(data => {
         const favourites = data.rows;
         console.log("fave", favourites);
         res.redirect("/favourites");
       });
+
 
   });
   return router;
