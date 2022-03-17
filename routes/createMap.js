c72fb4e5e6ebf46e86b1b157f41de01a515d17d7
Richard <au.richard4@gmail.com>
2022-03-17 12:22:40 -0700

    Added another db.query inside the .then to add the new map to contributions page.

--- routes/createMap.js
+++ routes/createMap.js
@@ -16,9 +16,12 @@ module.exports = (db) => {
     // const title = req.
     // console.log("Req Query and Body", req.query, req.body);
 
-    db.query(`INSERT INTO maps (title, user_id, latitude, longitude) VALUES ($1, $2, $3, $4)`, [req.body.title, userID, req.body.latitude, req.body.longitude])
+    db.query(`INSERT INTO maps (title, user_id, latitude, longitude) VALUES ($1, $2, $3, $4) RETURNING *;`, [req.body.title, userID, req.body.latitude, req.body.longitude])
       .then(data => {
+        console.log("return data rows", data.rows[0].id);
+        db.query(`INSERT INTO contributions (user_id, map_id) VALUES ($1, $2);`, [userID, data.rows[0].id]);
         res.redirect("/createMap");
+
       })
       .catch(err => {
         console.log("res error", err.message);
