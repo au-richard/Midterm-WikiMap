622da5aec1d9909b3a696631a2f2e28045613c85
Richard <au.richard4@gmail.com>
2022-03-16 18:24:58 -0700

    Took out join markers table. Selecting only maps.title instead of *.

--- routes/contributions.js
+++ routes/contributions.js
@@ -4,7 +4,8 @@ const router = express.Router();
 module.exports = (db) => {
   router.get("/", (req, res) => {
     const userID = req.session["user_id"];
-    db.query(`SELECT * FROM contributions JOIN users ON users.id = contributions.user_id JOIN maps ON maps.id = contributions.map_id WHERE contributions.user_id = $1;`, [userID])
+    // JOIN markers ON markers.user_id = contributions.user_id
+    db.query(`SELECT maps.title FROM contributions JOIN maps ON maps.id = contributions.map_id WHERE contributions.user_id = $1;`, [userID])
       .then(data => {
         const contributions = data.rows;
         console.log("contributions", contributions);
