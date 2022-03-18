05ac71f574e66d8de8abb785d3679174c615769e
Monther Balghonaim <monther.balghonaim@gmail.com>
2022-03-18 19:32:03 +0000

    fixed delete and edit buttons

--- routes/markers.js
+++ routes/markers.js
@@ -42,7 +42,7 @@ module.exports = (db) => {
     const {title, description, image_url, latitude, longitude} = req.body;
 
 
-    db.query(`INSERT INTO markers (title, description, image_url, user_id, map_id, latitude, longitude) value($1, $2, $3, $4, $5, $6, $7);`, [title,description,image_url, userID ,mapID, latitude, longitude])
+    db.query(`INSERT INTO markers (title, description, image_url, user_id, map_id, latitude, longitude) VALUES($1, $2, $3, $4, $5, $6, $7);`, [title,description,image_url, userID ,mapID, latitude, longitude])
       .then(data => {
         const markers = data.rows;
         console.log(markers);
